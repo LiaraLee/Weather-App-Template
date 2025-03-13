75a3c6aa9f52efa4a25b5938e2d92cc828b4e169
LiaraLee <kay.cecile.tite@gmail.com>
2025-03-13 04:11:06 -0400

    Displaying wether data w/ notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 function App() {
+  // const useState to store the weather data from the API
+  const [weather, setWeather] = useState(null);
+
   useEffect(() => {
     const getData = async () => {
       const APIKEY = import.meta.env.VITE_API_KEY;
       const url = `https://api.openweathermap.org/data/2.5/weather?q=New+York&appid=${APIKEY}`;
+
       try {
         const res = await fetch(url);
         if (!res.ok) {
@@ -12,7 +16,8 @@ function App() {
         }
         
         const data = await res.json();
-        console.log(data);
+        setWeather(data); // stores the data in state, i think this is hwere it updates the data too?
+        // console.log(data);
       } catch (error) {
         console.error(error.message);
       }
@@ -20,9 +25,18 @@ function App() {
     getData();
   }, []);
   return (
-    <div className="App">
+    // use className instead of class beacuse react doesnt like class beacuse it is a keyword for javascript and it messes up the code when react translates to javascript, add the div to wrap the whole beacuse react doesnt like multiple elements in the middle of a code
+     <div className="App">
+      {/* use brackets to wrap you comments inside of jsx? */}
       <header className="App-header">
         <h1>Weather</h1>
+        {/* // this checks to make sure all data is ready before it actualy renders it for users to see */}
+       {weather && ( 
+          <div>
+          <h2>{weather.name}</h2> {/*weather+city name*/}
+          <p>Temperature: {weather.main.temp}°C</p> {/*temperature displayed in celsius, maybe try to make it display both with a button?*/}
+          </div>
+       )}
       </header>
     </div>
   );
