77e68f38a9e0d72c1758eccff6fa07ec2b967b03
LiaraLee <kay.cecile.tite@gmail.com>
2025-03-13 06:31:27 -0400

    Removed all comments and updated formatting

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,31 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [city, setCity] =useState("New York");
+  const [city, setCity] = useState("New York");
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(true);
 
-      const getData = async () => {
-        setLoading(true);
-        const APIKEY = import.meta.env.VITE_API_KEY;
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`;
+  const getData = async () => {
+    setLoading(true);
+    const APIKEY = import.meta.env.VITE_API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`;
 
-      try {
-        const res = await fetch(url);
-        if (!res.ok) {
-          throw new Error(`Response status: ${res.status}`);
-        }
-        const data = await res.json();
-        setWeather(data);
-      } catch (error) {
-        console.error(error.message);
-      } finally {
-        setLoading(false);
+    try {
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw new Error(`Response status: ${res.status}`);
       }
-    };
-    useEffect(() => {
+      const data = await res.json();
+      setWeather(data);
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
     getData();
-    // this adds city as a dependancy for useEffect wich triggers a new fetch request every time the city changes
-  }, [city]); 
+  }, [city]);
 
   return (
     <div className="App">
@@ -37,7 +36,7 @@ function App() {
           type="text"
           value={city}
           onChange={(e) => setCity(e.target.value)}
-          placeholder="Enter city name" 
+          placeholder="Enter city name"
         />
         <button onClick={getData}>Get Weather</button>
         {loading && <p>Loading...</p>}
