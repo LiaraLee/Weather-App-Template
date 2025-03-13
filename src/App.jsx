dddd2f5995644ea24717dd33449a4d421895b16d
LiaraLee <kay.cecile.tite@gmail.com>
2025-03-13 05:18:25 -0400

    Added Loading Message

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+
 function App() {
   const [weather, setWeather] = useState(null);
+  // initializing a new loading state to true to show a loading message while the data is being fetched
+  const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
     const getData = async () => {
@@ -19,14 +22,21 @@ function App() {
       } catch (error) {
         console.error(error.message);
       }
+      finally {
+        // setting the loading state to false after the data has been fetched, or if an error occurs, basically turns off the loading message if there is an error or if the data has been fetched
+        setLoading(false);
+      }
     };
     getData();
   }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weather</h1>
-        {weather && (
+        {loading && <p>Loading...</p>}{/* this is where it actually displays a loading message for the user while the data is being fetched */}
+        {/* now we add !lading && to stop the lading message from displaying if the data has been fetched. basically we start with loading true and weather null then once the data is fetched weather becomes true and loading becomes null thus displaying your weather data and removing your loading message */}
+        {weather && !loading && ( 
           <div>
             <h2>{weather.name}</h2>
             <p>Temperature: {weather.main.temp}°C</p>
