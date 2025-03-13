a36e615f3cadb3cdf703d43947f0e7379cb8db05
LiaraLee <kay.cecile.tite@gmail.com>
2025-03-13 06:59:22 -0400

    API error message update, w/ comments

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ function App() {
   const [city, setCity] = useState("New York");
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null); // stores the error message
 
   const getData = async () => {
     setLoading(true);
+    setError(null); //resets the errror message before the new request
     const APIKEY = import.meta.env.VITE_API_KEY;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`;
 
@@ -19,7 +21,7 @@ function App() {
       const data = await res.json();
       setWeather(data);
     } catch (error) {
-      console.error(error.message);
+      setError(error.message); // store error message instead of console.log, this updates the state when an error happens
     } finally {
       setLoading(false);
     }
@@ -40,7 +42,9 @@ function App() {
         />
         <button onClick={getData}>Get Weather</button>
         {loading && <p>Loading...</p>}
-        {weather && !loading && (
+        {/* display the error message*/}
+        {error && <p>{error}</p>}
+        {weather && !loading && !error && (
           <div>
             <h2>{weather.name}</h2>
             <p>Temperature: {weather.main.temp}°C</p>
